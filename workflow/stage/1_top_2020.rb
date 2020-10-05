37728a809c4f40d950c1509f2814c70e7092927f
Yo Robot <gerald.bauer+yorobot@gmail.com>
2020-10-05 18:35:20 +0200

    up stage

--- workflow/stage/1_top_2020.rb
+++ workflow/stage/1_top_2020.rb
@@ -9,54 +9,68 @@ require 'convert'
 ##  sleep/wait 8secs after every request (should result in ~7-8 requests/minute)
 Webget.config.sleep = 8
 
-def download
-  Footballdata.eng( 2020 )   ## note. includes Premier League & Championship
-  Footballdata.de( 2020 )
-  Footballdata.es( 2020 )
-  Footballdata.fr( 2020 )
-  Footballdata.it( 2020 )
 
-  Footballdata.nl( 2020 )
-  Footballdata.pt( 2020 )
+DATASETS = [
+  ['eng.1', %w[2020/21]],
+  ['eng.2', %w[2020/21]],
 
-  Footballdata.br( 2020 )
+  ['de.1',  %w[2020/21]],
+  ['es.1',  %w[2020/21]],
+  ['fr.1',  %w[2020/21]],
+  ['it.1',  %w[2020/21]],
 
-  Footballdata.cl( 2020 )
+  ['nl.1',  %w[2020/21]],
+  ['pt.1',  %w[2020/21]],
+
+  ['br.1',  %w[2020]],
+
+  ['cl.1',  %w[2020/21]],
+]
+
+
+pp DATASETS
+
+
+
+###
+## todo/fix: (re)use / move into
+##     Footballdata::Tool.download( DATASETS ) !!!!
+##    or Footballdata::Batch.download  or Job.download or ?????
+def download( datasets )
+  datasets.each do |dataset|
+    league  = dataset[0]
+    seasons = dataset[1]
+    seasons.each do |season|
+      Footballdata.schedule( league: league,
+                             season: season )
+    end
 end
-
 
 
 Footballdata.config.convert.out_dir = './stage/one'
 
-def convert
-  ['ENG.1',
-   'ENG.2',
-   'DE.1',
-   'ES.1',
-   'FR.1',
-   'IT.1',
-   'NL.1',
-   'PT.1',
-   'BR.1',   ### note: gets 2020/21 season!! runs until february 2021!!!
-  ].each do |league|
-    Footballdata.convert( league: league, year: 2020 )
-  end
-
-  ## note: use special case converter for champions league for now
-  Footballdata.convert_cl( league: 'CL', year: 2020 )
+def convert( datasets )
+  datasets.each do |dataset|
+    league  = dataset[0]
+    seasons = dataset[1]
+    seasons.each do |season|
+      Footballdata.convert( league: league,
+                            season: season )
+    end
 end
+
 
 
 
 
 if $PROGRAM_NAME == __FILE__
   if ARGV.size == 0
-    convert()
+    convert( DATASETS )
   else
     ARGV.each do |arg|
       case arg
-      when 'dl', 'download'  then download()
-      when 'convert'         then convert()
+      when 'dl', 'download'  then download( DATASETS )
+      when 'convert'         then convert( DATASETS )
       else
         puts "[top script] unknown argument >#{arg}<"
         exit 1
